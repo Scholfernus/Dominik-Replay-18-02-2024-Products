591f379eee2dd5e265305bbc469f6be203ba994f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-02-20 22:36:12 +0100

    Zmiany do poprawnie działającej aplikacji - cały projekt

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product_model(name, price) VALUES ('Bike','99.99')
+INSERT INTO product_model(name, price) VALUES ('Boat','15000.0')
+INSERT INTO product_model(name, price) VALUES ('Ship','258000.99')
+INSERT INTO product_model(name, price) VALUES ('Car','35500.66')
+INSERT INTO product_model(name, price) VALUES ('Bus','250000.55')
+INSERT INTO product_model(name, price) VALUES ('Shoes','99.99')
